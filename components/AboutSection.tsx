f26725def945811d6e306b2a288fa8af16ccd621
Dominik Hoffmann <dominik151099@outlook.de>
2025-12-29 14:18:57 +0100

    feat: terminal about section

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -34,7 +34,6 @@ export default function AboutSection() {
                 <AnimatedSpan delay={1000} className="text-white">
                     <span>r-x------ 1 dh1542 dh1542 4.0K Dec 2 2024 <span className="font-bold text-blue-500">projects</span></span>
                 </AnimatedSpan>
-
                 <AnimatedSpan delay={1000} className="text-red-500">
                     /opt/dh1542.github.io ---&gt;
                 </AnimatedSpan>
@@ -44,7 +43,6 @@ export default function AboutSection() {
                 <AnimatedSpan delay={3000} className="text-white">
                     <span>Welcome to my portfolio website<br/> </span>
                 </AnimatedSpan>
-
                 <AnimatedSpan delay={3000} className="text-red-500">
                     /opt/dh1542.github.io ---&gt;
                 </AnimatedSpan>
@@ -52,13 +50,11 @@ export default function AboutSection() {
                     &gt;&gt;  cat ./skills/skill_list.txt
                 </TypingAnimation>
                 <AnimatedSpan delay={6000} className="text-white">
-
                         <ol>
                             <li>Java</li>
                             <li>React</li>
                             <li>....</li>
                         </ol>
-
                 </AnimatedSpan>
                 <AnimatedSpan delay={6000} className="text-red-500">
                     /opt/dh1542.github.io ---&gt;
@@ -67,18 +63,14 @@ export default function AboutSection() {
                     &gt;&gt;  cat ./work_experience/work.txt
                 </TypingAnimation>
                 <AnimatedSpan delay={9000} className="text-white">
-
                     <ol>
                         <li>ISO</li>
                         <li>usw. und so fort</li>
                         <li>....</li>
                     </ol>
-
                 </AnimatedSpan>
-
             </Terminal>
             </div>
-
         </div>
 
     )
